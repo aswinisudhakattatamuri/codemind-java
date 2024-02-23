eee93311af7aae4a6c8ffe0700cde7e41e042ebf
aswinisudhakattatamuri <119839899+aswinisudhakattatamuri@users.noreply.github.com>
2024-02-23 12:02:41 +0530

    import java.util.*; public class sol{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int X = sc.nextInt();         if(X<=(1000)/4)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sol{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int X = sc.nextInt();
+        if(X<=(1000)/4)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
