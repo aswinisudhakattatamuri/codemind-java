a44597040746913fe34213cad718e34ac6936d9c
aswinisudhakattatamuri <119839899+aswinisudhakattatamuri@users.noreply.github.com>
2024-03-04 11:22:45 +0530

    import java.util.*; public class Sol{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x>98)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }
        }
    }

---
+++ Fever.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>98)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+        
+    }
+}
