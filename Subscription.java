82b7047561da670e75f5ca1118b333447ba06d4f
aswinisudhakattatamuri <119839899+aswinisudhakattatamuri@users.noreply.github.com>
2024-03-04 12:02:48 +0530

    import java.util.*; public class SOl{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x>40)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SOl{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>40)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
