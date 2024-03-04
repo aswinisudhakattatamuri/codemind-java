703efb8e22f8852db51f22cf911e761fa15e18ad
aswinisudhakattatamuri <119839899+aswinisudhakattatamuri@users.noreply.github.com>
2024-03-04 10:40:25 +0530

    import java.util.*; public class Sol{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);          int x=sc.nextInt();          if(x>=18)          {              System.out.print("YES");          }          else          {              System.out.print("NO");          }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sol{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+         int x=sc.nextInt();
+         if(x>=18)
+         {
+             System.out.print("YES");
+         }
+         else
+         {
+             System.out.print("NO");
+         }
+    }
+}
